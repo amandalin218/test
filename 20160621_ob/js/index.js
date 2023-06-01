446b546b5c1aef04d67bb2e8e14efb292fd8862a
amandalin218 <113961556+amandalin218@users.noreply.github.com>
2023-06-01 10:35:50 +0800

    Add files via upload

---
+++ 20160621_ob/js/index.js
@@ -0,0 +1,39 @@
+$(function(){
+	$('.logo').click(function(){window.open('http://apps.gameflier.com/wx2/index.aspx');      });
+	$('.fb').click(function(){window.open('https://www.facebook.com/GFwx2/');      });
+    $('.fb2').click(function(){window.open('http://www.facebook.com/sharer.php?u= http://apps.gameflier.com/wx2/action/20160621_ob/index.html');      });
+	$('.save').click(function(){window.open('http://gf2.gameflier.com/mobile/billing/WX2/');      });
+    $('.qrcode').click(function(){window.open('https://www.facebook.com/GFwx2/');      });
+    $('.googleplay').click(function(){window.open('http://apps.gameflier.com/wx2/ReUrl/Android.aspx');      });	
+    $('.applestore').click(function(){window.open('http://apps.gameflier.com/wx2/ReUrl/ios.aspx');      });
+   		
+});
+$(function(){
+	$('.btn1').click(function(){
+		$('html,body').animate({scrollTop:$('#content3 ').offset().top}, 800);
+	}); 
+	$('.btn2').click(function(){
+		$('html,body').animate({scrollTop:$('#content4').offset().top}, 800);
+	});
+	$('.btn3').click(function(){
+		$('html,body').animate({scrollTop:$('#content5').offset().top}, 800);
+	});  
+	$('.btn4').click(function(){
+		$('html,body').animate({scrollTop:$('#content6').offset().top}, 800);
+	}); 
+	$('.btn5').click(function(){
+		$('html,body').animate({scrollTop:$('#content7').offset().top}, 800);
+	}); 
+	$('.btn6').click(function(){
+		$('html,body').animate({scrollTop:$('#content8').offset().top}, 800);
+	}); 
+	$('.btn7').click(function(){
+		$('html,body').animate({scrollTop:$('#content9').offset().top}, 800);
+	}); 
+	$('.btn8').click(function(){
+		$('html,body').animate({scrollTop:$('#content10').offset().top}, 800);
+	});
+	$('.top').click(function(){
+		$('html,body').animate({scrollTop:$('#content').offset().top}, 800);
+	});
+});
