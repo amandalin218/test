2858de7b288acd9e6283e228c1f5255fc8d03e9e
amandalin218 <113961556+amandalin218@users.noreply.github.com>
2023-06-01 13:34:18 +0800

    Add files via upload

---
+++ jan_20150115/js/index.js
@@ -0,0 +1,26 @@
+$(function(){
+	    $('#logo').click(function(){
+         window.open('http://aika.gameflier.com/');
+    });
+
+	    $('#download').click(function(){
+         window.open('http://aika.gameflier.com/download/download.aspx');
+    });
+	    $('#account').click(function(){
+         window.open('https://gfb.gameflier.com/Billing/account/AIKA/index2.asp?SourceID=2&Game=AIKA');
+    });
+	    $('#fb').click(function(){
+         window.open('https://www.facebook.com/aika.tw');
+    });
+	    $('#fbshare').click(function(){
+         window.open('http://www.facebook.com/sharer.php?u=http://aika.gameflier.com/action/jan_20150115/index.html ');
+    });
+	    $('#gamer').click(function(){
+         window.open('http://forum.gamer.com.tw/A.php?bsn=12079');
+    });
+	    $('#gamebase').click(function(){
+         window.open('http://www.gamebase.com.tw/forum/60086');
+    });
+
+
+});
