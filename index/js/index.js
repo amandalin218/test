1ff8d7713e7c453a691280e8ef735fd8a7f5f50e
amandalin218 <113961556+amandalin218@users.noreply.github.com>
2023-06-01 13:32:33 +0800

    Add files via upload

---
+++ index/js/index.js
@@ -0,0 +1,7 @@
+$(function(){
+	  	$('.logo').click(function(){window.open('http://tsm.chinesegamer.net/index');});
+		$('.fb').click(function(){window.open('https://www.facebook.com/TSM.Taiwan');});
+		$('.share').click(function(){window.open('http://www.facebook.com/sharer.php?u=https://m-event.chinesegamer.net/tsm/mycard_0917/');});
+		$('.btn').click(function(){window.open('');});
+		
+	});
